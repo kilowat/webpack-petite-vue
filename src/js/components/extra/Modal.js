036a81c132d45eafd5302a649f6e98b116fd110c
Kilowat <kadantsev.sergey@gmail.com>
2022-06-15 23:12:52 +0300

    work

--- src/js/components/extra/Modal.js
+++ src/js/components/extra/Modal.js
@@ -1,4 +1,4 @@
-import { reactive } from 'petite-vue'
+import { reactive, nextTick  } from 'petite-vue'
 
 const state = reactive({
   items:{},
@@ -7,28 +7,66 @@ const state = reactive({
     if (!this.items[id]) this.items[id] = {};
     this.items[id].$el = $el;
     this.items[id].data = data || {};
+  },
+  getEl(id) {
+    return state.items[id].$el;
   }
 })
 
+
 export default {
-  create(id, data){
-    state.init(id, data);
+  create(props, data){
+    state.init(props.id, data);
+    const width = props.width || 640;
+    const $el = state.getEl(props.id);
+    $el.setAttribute('aria-hidden', false);
+    $el.classList.add('m-close');
+    const template = `
+    <div v-scope class="modal-root" @click="Modal.close('${props.id}')">
+      <div class="modal-bg">
+        <div class="modal-window" @click.stop="" :style="{maxWidth: width + 'px'}">
+          ${$el.innerHTML}
+          {{width}}
+        </div>
+      </div>
+    </div>`
+
     return {
-      get data() {return state.items[id].data},
+      $template: template,
+      get data() {return state.items[props.id].data},
+      show: false,
+      width,
+      mounted() {
+        $el.remove();
+      },
     }
   },
   show(id, data) {
+    console.log('show');
+    this.closeAll(id);
+    const $el = state.getEl(id);
+
     if (!state.items[id]) throw Error(`Modal with id ${id} not found`);
     if (data) state.items[id].data = data;
-    state.items[id].$el.setAttribute('aria-hidden', false);
+    if (!document.querySelector('#'+id)) document.querySelector('body').appendChild($el);
+    
+    $el.classList.remove('m-close');
+    $el.classList.add('m-open');
+    document.querySelector('body').style.overflow = 'hidden';
   },
   close(id) {
     if (!state.items[id]) throw Error(`Modal with id ${id} not found`);
-    state.items[id].data = {};
-    state.items[id].$el.setAttribute('aria-hidden', true);
+    const $el = state.getEl(id);
+    
+    if  ($el.classList.contains('m-open')) {
+      $el.classList.remove('m-open');
+      $el.classList.add('m-close');
+      document.querySelector('body').style.overflow = 'auto';
+    }
   },
-  closeAll() {
+  closeAll(exeptId) {
     for(const i in state.items) {
+      if(exeptId === i) continue;
       this.close(i);
     }
   }
