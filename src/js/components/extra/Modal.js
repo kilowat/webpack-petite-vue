3cd3f187dfca425e7bd3082418c93fd2c7b76304
Kilowat <kadantsev.sergey@gmail.com>
2022-06-15 14:03:26 +0300

    work with modal

--- src/js/components/extra/Modal.js
+++ src/js/components/extra/Modal.js
@@ -1,7 +1,35 @@
-export default (props) => {
-  return {
-    show() {
-      console.log('show')
+import { reactive } from 'petite-vue'
+
+const state = reactive({
+  items:{},
+  init(id, data) {
+    const $el = document.querySelector('#'+id);
+    if (!this.items[id]) this.items[id] = {};
+    this.items[id].$el = $el;
+    this.items[id].data = data || {};
+  }
+})
+
+export default {
+  create(id, data){
+    state.init(id, data);
+    return {
+      get data() {return state.items[id].data},
+    }
+  },
+  show(id, data) {
+    if (!state.items[id]) throw Error(`Modal with id ${id} not found`);
+    if (data) state.items[id].data = data;
+    state.items[id].$el.setAttribute('aria-hidden', false);
+  },
+  close(id) {
+    if (!state.items[id]) throw Error(`Modal with id ${id} not found`);
+    state.items[id].data = {};
+    state.items[id].$el.setAttribute('aria-hidden', true);
+  },
+  closeAll() {
+    for(const i in state.items) {
+      this.close(i);
     }
   }
-}
+};
--- src/js/main.js
+++ src/js/main.js
@@ -2,19 +2,25 @@
 import { createApp } from 'petite-vue'
 import components from './components'
 import store from './store'
+import directives from './directives';
 import EventEmitter from './utils/EventEmitter';
-
 
 const settings = {
   store,
   ...components,
 }
+const vapp = createApp(settings)
 const eventBus = new EventEmitter();
+
+for(const i in directives) {
+  vapp.directive(i, directives[i]);
+}
+
 window.eventBus = eventBus;
-window.vapp = createApp(settings).mount('#app')
+window.vapp = vapp.mount('#app');
 
 document.addEventListener("DOMContentLoaded", function(event) { 
-  // to do
+  //to do
 });
 
 
