23a619013c26fe350abb9b74608f74f568fdc088
Kilowat <kadantsev.sergey@gmail.com>
2022-07-28 21:24:45 +0300

    work

--- src/js/components/app/news/NewsList.js
+++ src/js/components/app/news/NewsList.js
@@ -3,10 +3,12 @@ export default () => {
   let $root;
   
   return {
+    loaded: false,
     init($el) {
       $root = $el;
     },
     async loadMore() {
+      this.loaded = true;
       const res = await fetch('/ajax.html');
       const htmlRes = await res.text();
       const html = new DOMParser().parseFromString(htmlRes, "text/html").body;
@@ -15,7 +17,7 @@ export default () => {
       for (let i = 0; i < newsItems.length; i++) {
         container.appendChild(newsItems[i])
       }
-      window?.eventBus.emit('remount');
+      window?.eventBus?.emit('remount');
     }
   }
 }
